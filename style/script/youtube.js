06fcc081c9f7e708d5d1891f35d501437e5e5abc
sharmila k <sharmilakaruppanan3@gmail.com>
2022-02-28 19:19:24 +0530

    function added

---
+++ style/script/youtube.js
@@ -0,0 +1,34 @@
+
+function setOldImage() {
+document.getElementById("v1").src = "https://i.ytimg.com/an_webp/-Nw7yRunFbY/mqdefault_6s.webp?du=3000&sqp=CISHvJAG&rs=AOn4CLASsjKy1Q2TebZO0i1pjM8kyY5QDA";
+}
+function setNewImage() {
+document.getElementById("v1").src = "";
+}
+
+function setOldImage2() { 
+
+document.getElementById("v2").src = "https://i.ytimg.com/an_webp/Hwphc4fQu6Y/mqdefault_6s.webp?du=3000&sqp=CNzOu5AG&rs=AOn4CLAQk1bQYoZPziDjB-XIUBzxfLQi2Q";
+
+}
+function setNewImage2() {
+document.getElementById("v2").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmHt7YiYTo7FzTzFWGskcXlLeww6fPx_rkiA&usqp=CAU";
+}
+
+function setOldImage3() {
+document.getElementById("v3").src = "https://i.ytimg.com/an_webp/N251yeT_cQQ/mqdefault_6s.webp?du=3000&sqp=CMDmu5AG&rs=AOn4CLCepCxQadIqDYkreASI_a6YNnLJ1g";
+
+
+}
+
+function setNewImage3() {
+document.getElementById("v3").src = "https://encrypted-Nd9GcL-O_XW3q7A&usqp=CAU";
+}
+
+function setOldImage4() {
+document.getElementById("v4").src = "https://i.ytimg.com/an_webp/rtrV-RY_-wo/mqdefault_6s.webp?du=3000&sqp=CMCDvJAG&rs=AOn4CLDk7zGO4aDNQPXX8z7bbZdZE6kvXA";
+}
+function setNewImage4() {
+
+document.getElementById("v4").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFeAyff6iNWf8b9L6BeCLWEjcEVpT6uOw3KA&usqp=CAU";
+}
